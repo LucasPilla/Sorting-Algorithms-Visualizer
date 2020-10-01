0743209a42cdeb5cc363fedbb7e67a5eea9eec4b
Luc Blassel <luc.blassel@gmail.com>
2020-10-01 18:20:50 +0200

    adding heapSort

--- algs.py
+++ algs.py
@@ -119,6 +119,38 @@ def cocktailSort(array, *args):
                 array[i], array[i+1] = array[i+1], array[i]
                 swapped = True
         start = start+1
+
+
+def heapify(array, count):
+    start = (count-1) // 2
+    while start >= 0:
+        siftDown(array, start, count - 1)
+        start -= 1
+
+def siftDown(array, start, end):
+    root = start
+    while 2 * root + 1 <= end:
+        child = 2 * root + 1
+        swap = root
+        if array[swap] < array[child]:
+            swap = child
+        if child + 1 <= end and array[swap] < array[child + 1]:
+            swap = child + 1
+        if swap == root:
+            return
+        else:
+            handleDrawing(array, root, swap, -1, -1)
+            array[root], array[swap] = array[swap], array[root]
+            root = swap
+
+def heapSort(array, *args):
+    heapify(array, len(array))
+    end = len(array) - 1
+    while end > 0:
+        handleDrawing(array, -1, -1, 0, end, )
+        array[end], array[0] = array[0], array[end]
+        end -= 1
+        siftDown(array, 0, end)
         
         
 algorithmsDict = {
@@ -129,6 +161,7 @@ algorithmsDict = {
     'quicksort': quickSort,
     'countingsort': countingSort,
     'cocktailsort': cocktailSort,
+    'heapsort': heapSort,
 }
 
 
