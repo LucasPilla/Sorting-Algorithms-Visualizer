4d418ea9037407530647a781b0302de0f6735336
Utsav <utsavp0213@gmail.com>
2020-10-03 23:37:18 +0530

    Refactored the code

---
+++ algorithms/__init__.py
@@ -0,0 +1,11 @@
+from algorithms.bogoSort import bogoSort
+from algorithms.bubbleSort import bubbleSort
+from algorithms.cocktailSort import cocktailSort
+from algorithms.countingSort import countingSort
+from algorithms.heapSort import heapSort
+from algorithms.insertionSort import insertionSort
+from algorithms.mergeSort import mergeSort
+from algorithms.quickSort import quickSort
+from algorithms.selectionSort import selectionSort
+
+__all__ = ['bogoSort', 'bubbleSort', 'cocktailSort', 'countingSort', 'heapSort', 'insertionSort', 'mergeSort', 'quickSort', 'selectionSort']
---
+++ algorithms/insertionSort.py
@@ -0,0 +1,12 @@
+from display import handleDrawing
+
+def insertionSort(array, *args):
+    size = len(array)
+    for i in range(1, size):
+        j = i-1
+        key = array[i]
+        while j >= 0 and array[j] > key:
+            handleDrawing(array, j, -1, i, -1)
+            array[j+1] = array[j]
+            j -= 1
+        array[j+1] = key
