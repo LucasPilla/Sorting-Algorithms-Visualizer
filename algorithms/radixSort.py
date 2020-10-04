621535d123640be9ef8b963ccbdc354edf3cf594
aswarth123 <53229329+aswarth123@users.noreply.github.com>
2020-10-04 12:30:14 +0530

    Update radixSort.py

--- algorithms/radixSort.py
+++ algorithms/radixSort.py
@@ -3,7 +3,9 @@ from display import handleDrawing
 def counting_Sort(array, exp1):
 
     n = len(array)
-    output = [0] * (n)
+    output=[]
+    for  i in range (0,n):
+        output.append(array[i])
     count = [0] * (10)
     for i in range(0, n):
         index = (array[i] / exp1)
@@ -13,19 +15,26 @@ def counting_Sort(array, exp1):
     i = n - 1
     while i >= 0:
         index = (array[i] / exp1)
-        handleDrawing(array,count[int(index % 10)] - 1,-1,i,-1)
+        handleDrawing(output,count[int(index % 10)]-1,-1,int(index % 10),-1)
         output[count[int(index % 10)] - 1] = array[i]
         count[int(index % 10)] -= 1
         i -= 1
     i = 0
+    if(array!=output):
+        pass
+    else:
+        return(0)
     for i in range(0, len(array)):
-        arr[i] = output[i]
+        array[i] = output[i]
+    del(output)
 
 def radixSort(array, *args):
 
     max1 = max(array)
-
+    g=1
     exp = 1
     while max1 / exp > 0:
-        counting_Sort(array, exp)
+        g=counting_Sort(array, exp)
+        if(g==0):
+            break
         exp *= 10
