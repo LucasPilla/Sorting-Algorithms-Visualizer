1794d6a077a8cae8767363cdbc39487f6c4b1600
Alejandro Antonio Olaria Robertis <alejandro.olaria@gmail.com>
2020-10-11 13:19:19 -0300

    combsort algorithm done

--- algorithms/combSort.py
+++ algorithms/combSort.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# code: utf-8
-from display import handleDrawing
+# coding: utf-8
+#from display import handleDrawing
 
 
 def get_gap(prev_gap=None, a_length=None) -> int:
@@ -20,12 +20,35 @@ def get_gap(prev_gap=None, a_length=None) -> int:
     if isinstance(a_length, int) is False:
         raise TypeError("\'a_length\' must be \'int\' type")
 
-    gap = int((prev_gap * a_length)/1.3)
+    gap = int((prev_gap * a_length)/13)
 
     if gap < 1:
         return 1
     return gap
     
 
-a = get_gap(10, 10)
-print(a)
+def combSort(_list=None) -> list:
+    """
+    Sort a list using \'Comb Sort\'
+    :param _list: list with real numbers
+    :return: sorted list
+    """
+    if _list is None:
+        raise ValueError("a list of numbers is needed")
+    if isinstance(_list, list) is False:
+        raise TypeError("a \'_list\' must be \'list\' type")
+    if len(_list) is False:
+        raise ValueError("list is empty")
+    
+    a_length = len(_list)
+    gap = a_length
+    swapped = True
+
+    while gap != 1 or swapped:
+        gap = get_gap(prev_gap=gap, a_length=len(_list))
+        swapped = False
+
+        for idx in range(0, a_length - gap):
+            if _list[idx] > _list[idx + gap]:
+                _list[idx], _list[idx + gap] = _list[idx + gap], _list[idx]
+                swapped = True
