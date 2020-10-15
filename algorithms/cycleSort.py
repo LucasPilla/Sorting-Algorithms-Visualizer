8b3963ee25c18fbd1c572539a68939c57767610c
Lucas Pilla Pimentel <64286128+LucasPilla@users.noreply.github.com>
2020-10-15 14:45:11 -0300

    Update cycleSort.py

--- algorithms/cycleSort.py
+++ algorithms/cycleSort.py
@@ -2,14 +2,11 @@ from display import handleDrawing
 
 
 def cycleSort(array, *args):
-    #swaps = 0
-
     for cycle_start in range(0, len(array) - 1):
         item = array[cycle_start]
-
         pos = cycle_start
-        item = array[pos]
-        for i in range(pos + 1, len(array)):
+        
+        for i in range(cycle_start + 1, len(array)):
             if array[i] < item:
                 pos += 1
 
@@ -18,24 +15,14 @@ def cycleSort(array, *args):
 
         while array[pos] == item:
             pos += 1
-
-        array[pos], array[cycle_start] = array[cycle_start], array[pos]
-        handleDrawing(array, cycle_start, pos, -1, -1)
-        #swaps = swaps+1
+        array[pos], item = item, array[pos]
 
         while pos != cycle_start:
             pos = cycle_start
-            item = array[pos]
-            for i in range(pos + 1, len(array)):
+            for i in range(cycle_start + 1, len(array)):
                 if array[i] < item:
                     pos += 1
 
-            if pos == cycle_start:
-                continue
-
             while array[pos] == item:
                 pos += 1
-
-            array[pos], array[cycle_start] = array[cycle_start], array[pos]
-            handleDrawing(array, cycle_start, pos, -1, -1)
-            #swaps = swaps + 1
+            array[pos], item = item, array[pos]
