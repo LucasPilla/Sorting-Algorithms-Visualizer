a28c092088d8be6bc5cac7c1b3110b196a7d205e
Lucas Pilla Pimentel <64286128+LucasPilla@users.noreply.github.com>
2020-10-23 09:19:10 -0300

    Update visualization
    Removed some uses of handledrawing() and tryed to improve visualization

--- algorithms/binaryinsertionSort.py
+++ algorithms/binaryinsertionSort.py
@@ -4,14 +4,11 @@ from display import handleDrawing
 def binaryinsertionSort(array, *args):
     for i in range(1, len(array)):
         val = array[i]
-        handleDrawing(array, i, -1, -1, -1)
-        j = binary_search(array, val, 0, i - 1)
+        j = binary_search(array, val, 0, i - 1, i)
         array[0:len(array)] = array[:j] + [val] + array[j:i] + array[i + 1:]
-        handleDrawing(array, i+1, -1, j, -1)
 
 
-def binary_search(arr, val, start, end):
-    handleDrawing(arr, -1, -1, start, end)
+def binary_search(arr, val, start, end, current):
     if start == end:
         if arr[start] > val:
             return start
@@ -22,9 +19,10 @@ def binary_search(arr, val, start, end):
         return start
 
     mid = round((start + end) / 2)
+    handleDrawing(arr, start, end, mid, current)
     if arr[mid] < val:
-        return binary_search(arr, val, mid + 1, end)
+        return binary_search(arr, val, mid + 1, end, current)
     elif arr[mid] > val:
-        return binary_search(arr, val, start, mid - 1)
+        return binary_search(arr, val, start, mid - 1, current)
     else:
         return mid
