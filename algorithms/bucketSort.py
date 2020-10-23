ab906079d2d80d984288d43b2ba97ebc0a69fa23
niharika11031997 <68109619+niharika11031997@users.noreply.github.com>
2020-10-23 12:02:23 +0530

    updated "algortihms" (added bucketSort)

---
+++ algorithms/bucketSort.py
@@ -0,0 +1,24 @@
+from display import handleDrawing
+
+def bucketSort(array, *args):
+ 
+    bucket = []
+    for i in range(len(array)):
+        bucket.append([])
+    
+    for j in array:
+        index_b = int(10/j)
+        handleDrawing(array,j,-1,index_b,-1)
+        bucket[index_b].append(j)
+        
+    
+    for i in range(len(array)):
+        #handleDrawing(array,i,-1,bucket[i],-1)
+        bucket[i]=sorted(bucket[i])
+    
+    k=0
+    for i in range(len(array)):
+        for j in range(len(bucket[i])):
+            handleDrawing(array,j,-1,i,-1)
+            array[k]=bucket[i][j]
+            k=k+1
