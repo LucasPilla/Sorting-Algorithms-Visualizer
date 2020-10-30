5c841e500547d8d9eac5bea4862d880acf8f1dcc
Lukas Winkler <lukas.winkler@cloudflight.io>
2020-10-30 17:22:31 +0100

    Improve readability by reducing line length

--- main.py
+++ main.py
@@ -26,7 +26,11 @@ def main():
             if display.startButton.active and not error_checking():
                 # Set the values given by the user
                 display.numBars = int(display.sizeBox.text)
-                display.delay = display.delayBox.value - display.delayBox.rect.x - 6
+                display.delay = (
+                    display.delayBox.value
+                    - display.delayBox.rect.x
+                    - 6
+                )
                 algorithm = display.algorithmBox.text
                 # Generates a random list
                 numbers = randomList()
