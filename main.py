b0ed787877c8d75ff89858a2ea53ea51da36438e
GlacialBlaze <67182916+GlacialBlaze@users.noreply.github.com>
2021-08-26 00:14:28 +0800

    Revert "Update main.py"

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 import display
 import pygame
-from algs   import algorithmsDict, runAlgorithm
-from random import randint, sample
+from algs import algorithmsDict, runAlgorithm
+from random import randint
 
+# Global Variables: numBars, delay, toDraw, button
+# They were declared in display.py
 
-# Global variables 'numBars', 'delay', 'toDraw', and 'button' were declared in display.py
 
 def main():
     numbers = []
+    running = True
     display.algorithmBox.add_options(list(algorithmsDict.keys()))
     
-    while True:
+    while running:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: return pygame.quit()
+            if event.type == pygame.QUIT:
+                running = False
 
             display.sizeBox.update(event)
             display.delayBox.update()
             display.algorithmBox.update()
             display.startButton.update()
+            a_set = set(range(display.numBars))
 
             if display.startButton.active:
-                display.numBars = int(display.sizeBox.text)          # Set the values given by the user
-                display.delay   = display.delayBox.value - display.delayBox.rect.x - 6
+                # Set the values given by the user
+                display.numBars = int(display.sizeBox.text)
+                display.delay =\
+                    display.delayBox.value - display.delayBox.rect.x - 6
                 algorithm = display.algorithmBox.get_active_option()
-                numbers   = sample(range(10, 400), display.numBars)  # Generates a random list
-                runAlgorithm(algorithm.lower(), numbers)             # Executes the chosen algorithm
+                # Generates a random list
+                numbers = [randint(10, 400) for i in range(display.numBars)]
+                # Executes the chosen algorithm
+                runAlgorithm(algorithm.lower(), numbers)
                 display.toDraw = True
-                
-        display.drawInterface(numbers, -1, -1, -1, -1, greenRows=set(range(display.numBars)))
+        display.drawInterface(numbers, -1, -1, -1, -1, greenRows = a_set)
 
 if __name__ == '__main__':
     main()
