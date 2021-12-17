937342ddd83803aceef9c75e7036d5fb01f3620e
GlacialBlaze <67182916+GlacialBlaze@users.noreply.github.com>
2021-12-17 23:27:14 +0800

    Updated file names for calling

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,50 +1,51 @@
-from algorithms.bubbleSort import bubbleSort
-from algorithms.bogoSort import bogoSort
-from algorithms.bitonicSort import bitonicSort
-from algorithms.binaryinsertionSort import binaryinsertionSort
-from algorithms.heapSort import heapSort
-from algorithms.gnomeSort import gnomeSort
-from algorithms.cycleSort import cycleSort
-from algorithms.countingSort import countingSort
-from algorithms.combSort import combSort
-from algorithms.cocktailSort import cocktailSort
-from algorithms.bucketSort import bucketSort
-from algorithms.timSort import timSort
-from algorithms.strandSort import strandSort
-from algorithms.stoogeSort import stoogeSort
-from algorithms.shellSort import shellSort
-from algorithms.selectionSort import selectionSort
-from algorithms.quickSort import quickSort
-from algorithms.pigeonholeSort import pigeonholeSort
-from algorithms.pancakeSort import pancakeSort
-from algorithms.oddevenSort import oddevenSort
-from algorithms.mergeSort import mergeSort
-from algorithms.insertionSort import insertionSort
-from algorithms.radixSort import radixSort
+from bubbleSort          import bubbleSort
+from bogoSort            import bogoSort
+from bitonicSort         import bitonicSort
+from binaryinsertionSort import binaryinsertionSort
+from heapSort            import heapSort
+from gnomeSort           import gnomeSort
+from cycleSort           import cycleSort
+from countingSort        import countingSort
+from combSort            import combSort
+from cocktailSort        import cocktailSort
+from bucketSort          import bucketSort
+from timSort             import timSort
+from strandSort          import strandSort
+from stoogeSort          import stoogeSort
+from shellSort           import shellSort
+from selectionSort       import selectionSort
+from quickSort           import quickSort
+from pigeonholeSort      import pigeonholeSort
+from pancakeSort         import pancakeSort
+from oddevenSort         import oddevenSort
+from mergeSort           import mergeSort
+from insertionSort       import insertionSort
+from radixSort           import radixSort
 
 
-__all__ = [ 
-    "bogoSort", 
-    "bitonicSort", 
-    "binaryinsertionSort", 
-    "bubbleSort", 
-    "heapSort", 
-    "gnomeSort", 
-    "cycleSort", 
-    "countingSort", 
-    "combSort", 
-    "cocktailSort", 
-    "bucketSort", 
-    "timSort", 
-    "strandSort", 
-    "stoogeSort", 
-    "shellSort", 
-    "selectionSort", 
-    "quickSort", 
-    "pigeonholeSort", 
-    "pancakeSort", 
-    "oddevenSort", 
-    "mergeSort", 
-    "insertionSort", 
-    "radixSort"
-]
+__all__ = ['bogoSort',
+           'bitonicSort',
+           'binaryinsertionSort',
+           'bubbleSort',
+           'heapSort',
+           'gnomeSort',
+           'cycleSort',
+           'countingSort',
+           'combSort',
+           'cocktailSort',
+           'bucketSort',
+           'timSort',
+           'strandSort',
+           'stoogeSort',
+           'shellSort',
+           'selectionSort',
+           'quickSort',
+           'pigeonholeSort',
+           'pancakeSort',
+           'oddevenSort',
+           'mergeSort',
+           'insertionSort',
+           'radixSort',
+           'exchangeSort']
+
+
