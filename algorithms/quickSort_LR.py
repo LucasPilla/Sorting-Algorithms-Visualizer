1218f476b95d65fe73a7db571dab5a012afefd75
GlacialBlaze <67182916+GlacialBlaze@users.noreply.github.com>
2021-12-18 15:30:14 +0800

    Update quickSort_LR.py

--- algorithms/quickSort_LR.py
+++ algorithms/quickSort_LR.py
@@ -1,21 +1,20 @@
-def quickSort_LR(array, low, high):
-    if low < high:
-        p = partition(array, low, high)
-        yield array, p, high, low, -1
-        yield from quickSort_LR(array, low, p)
-        yield from quickSort_LR(array, p + 1, high)
+from random import randint
 
-def partition(array, low, high):
-    pivot = array[low]
-    i     = low - 1
-    j     = high + 1
-    
-    while True:
-        i += 1
-        while array[i] < pivot: i += 1
+def quickSort_LR(array, low, high):
+    if high - low > 0:
+        pivot, left, right = array[randint(low, high)], low, high
         
-        j -= 1
-        while array[j] > pivot: j -= 1
+        while left <= right:
+            while array[left]  < pivot: left  += 1
+            while array[right] > pivot: right -= 1
+            
+            if left <= right:
+                array[left], array[right] = array[right], array[left]
+                left += 1
+                right -= 1
+            
+            yield array, pivot, high, low, -1
+            
+        yield from quickSort_LR(array, low, right)
+        yield from quickSort_LR(array, left, high)
         
-        if i >= j: return j
-        array[i], array[j] = array[j], array[i]
