4c0ba43e762de4af3397542ad5ba3dc616941e65
Jason Ona <jason.ona@cogent.co>
2022-05-07 21:09:26 +1000

    Refactored Pigeonhole Sort

--- algorithms/pigeonholeSort.py
+++ algorithms/pigeonholeSort.py
@@ -1,20 +1,34 @@
+# for pigeonholes, the indexes represent:
+# the value from the array - the minimum value,
+# and the item is the number of times that value is in the array
+# we initialize the counts to 0
+def pigeonholes(size):
+  return [0]*size
+
+def addTo(array, value):
+  array[value] += 1
+
+def subtractFrom(array, value):
+  array[value] -= 1
+
 def pigeonholeSort(array, *args):
-  minV = min(array)
-  maxV = max(array)
-  A = array.copy()
-  size = maxV - minV + 1
-  C = [0]*(size)
-  i = 0
-  for x in array:
-    yield A, i, -1, -1, -1
-    C[x - minV] += 1
-    A[i] = A[i] / (C[x-minV] + 1)
-    i += 1
-  i = 0
-  for count in range(size):
-    while C[count] > 0:
-      yield A, i-1, -1, C[count] * minV , -1
-      C[count] -= 1
-      array[i] = count + minV
-      A[i] = array[i]
-      i += 1
+  minValue = min(array)
+  maxValue = max(array)
+  rangeOfPossiblePigeonholes = maxValue - minValue + 1
+  COUNTS = pigeonholes(rangeOfPossiblePigeonholes)
+
+  # this yields array to be displayed and then
+  # this counts each value in the "pigeonhole" array
+  for step, value in enumerate(array):
+    yield array, step, -1, -1, -1
+    addTo(COUNTS, value - minValue)
+
+  # this yields array to be displayed and then
+  # this copies each value from the "pigeonhole" array
+  step = -1
+  for value in range(rangeOfPossiblePigeonholes):
+    while COUNTS[value] > 0:
+      yield array, step, -1, -1, -1
+      subtractFrom(COUNTS, value)
+      array[step] = value + minValue
+      step += 1
