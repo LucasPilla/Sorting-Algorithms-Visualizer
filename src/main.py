468dff7f559075ca564b22c1c24c6151f59fb257
Alexander <>
2022-11-03 22:18:07 +0100

    Added (very) basic gif function - will now output a gif

--- src/main.py
+++ src/main.py
@@ -3,11 +3,24 @@ from random import randint
 from time import time
 from algs import algorithmsDict
 import display as display
+import os
+import imageio.v3
 
 # Declared in display.py
 # 1. global variables : numBars, delay, do_sorting, paused, timer_space_bar
 # 2. widgets : sizeBox, delayBox, algorithmBox, playButton, stopButton
 
+
+SCREENSHOT_FILENAME = "screenshot" #+ a counter number
+GIF_WINDOW_SIZE = (900, 420)
+
+def getMaxNumber(files):
+    currentMax  = -1
+    for item in files:
+        myNumber = int(item[len(SCREENSHOT_FILENAME):len(item)-4])
+        if myNumber > currentMax:
+            currentMax = myNumber
+    return currentMax
 
 def main():
     numbers = []
@@ -18,7 +31,7 @@ def main():
     alg_iterator = None
 
     timer_delay = time()
-    
+    counter = 0
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -49,13 +62,42 @@ def main():
                     numbers, redBar1, redBar2, blueBar1, blueBar2 = next(alg_iterator)
             except StopIteration:
                 pass
-
+            #Check if pictures are there, and if so then output gif
+            #Check if user wants GIF
+            print("No gif check, that's not good!")
+            if True: #ADD SOME KIND OF GIF CHECK
+                #Idea is that pictures are generated with numbers 0 to some MAX
+                maxNumber = counter#So find MAX
+                print(str(maxNumber))
+                fileNames = [] #Okay, let's start preparing for GIF
+                for i in range(0,maxNumber):
+                    fileNames.append(SCREENSHOT_FILENAME + str(i) + ".jpg")
+                images = []
+                #This will start to load in individual pictures into gif engine
+                for filename in fileNames:
+                    images.append(imageio.v2.imread(filename))
+                #Output gif!
+                #imageio.mimsave("test", images)
+                imageio.mimsave('sorting.gif', images, format = 'GIF-PIL', fps = 100)
+                #Delete all files in folder
+                print("Attempting to delete temporary files!")
+                for i in range(0,maxNumber):
+                    try:
+                        os.remove(SCREENSHOT_FILENAME + str(i) + ".jpg")
+                    except:
+                        print("IO error, will attempt to continue!")
+            counter = 0
+        #Initialize screenshot area        
+        screenshot = pygame.Surface(GIF_WINDOW_SIZE)
+        screenshot.blit(display.screen, (0,0))
         if display.do_sorting and not display.paused: # sorting animation
             try:
                 if time()-timer_delay >= display.delay:
                     numbers, redBar1, redBar2, blueBar1, blueBar2 = next(alg_iterator)
                     display.drawInterface(numbers, redBar1, redBar2, blueBar1, blueBar2)
+                    pygame.image.save(screenshot, "screenshot" + str(counter) + ".jpg")
                     timer_delay = time()
+                    counter += 1
             except StopIteration:
                 display.do_sorting = False
         elif display.do_sorting and display.paused: # animation paused
