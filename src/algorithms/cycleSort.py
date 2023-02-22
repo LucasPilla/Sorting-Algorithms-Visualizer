a94d4574035bde88281a5c4df14074054de1e486
Lucas Rafaldini <lrafaldini@pm.me>
2023-02-22 00:14:13 -0300

    Add docstrings for cycleSort

--- src/algorithms/cycleSort.py
+++ src/algorithms/cycleSort.py
@@ -1,4 +1,22 @@
 def cycleSort(array, *args):
+    """
+    Sorts the given array using Cycle Sort Algorithm.
+
+    Cycle Sort is an in-place sorting algorithm that is most useful 
+    for situations where memory writes are more expensive than reads. 
+    It minimizes the number of memory writes to sort an array. It works 
+    by dividing the array into cycles, where each cycle contains one value 
+    that is in its correct position. The algorithm then moves through the cycles, 
+    swapping values until the entire array is sorted.
+
+    Args:
+        array (list): A list of comparable elements to be sorted.
+        *args: Variable length argument list.
+
+    Yields:
+        tuple: A tuple containing current state of array, current position of first element in the cycle, 
+        current position of the element which is being placed at its correct position, -1, -1.
+    """
     for cycle_start in range(0, len(array) - 1):
         item = array[cycle_start]
         pos = cycle_start
