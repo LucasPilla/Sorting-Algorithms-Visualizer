56989348a88a58ed6844aa3daaaf149a340f8341
Lucas Rafaldini <lrafaldini@pm.me>
2023-02-22 00:19:17 -0300

    Add docstrings for pigeonholeSort

--- src/algorithms/pigeonholeSort.py
+++ src/algorithms/pigeonholeSort.py
@@ -1,4 +1,24 @@
 def pigeonholeSort(array, *args):
+  """
+  Sorts an array in ascending order using Pigeonhole Sort Algorithm.
+
+  Pigeonhole Sort is a sorting algorithm that is used to sort items 
+  when they are limited to a range of values. The algorithm works by 
+  placing each item in its corresponding pigeonhole. Then, the items 
+  are sorted by finding the pigeonholes that contain items, in ascending 
+  order. It is useful when the range of values in the array is small 
+  compared to the size of the array, as it is a linear time sorting algorithm.
+
+  Args:
+    array: An array of integers to be sorted.
+    *args: Optional arguments.
+
+  Yields:
+    Tuple containing the current state of the array, indices of the two elements being compared, and indices of the
+    elements being modified.
+
+  """
+
   minV = min(array)
   maxV = max(array)
   A = array.copy()
@@ -10,11 +30,12 @@ def pigeonholeSort(array, *args):
     C[x - minV] += 1
     A[i] = A[i] / (C[x-minV] + 1)
     i += 1
+
   i = 0
   for count in range(size):
     while C[count] > 0:
-      yield A, i-1, -1, C[count] * minV , -1
-      C[count] -= 1
-      array[i] = count + minV
-      A[i] = array[i]
-      i += 1
+        yield A, i-1, -1, C[count] * minV , -1
+        C[count] -= 1
+        array[i] = count + minV
+        A[i] = array[i]
+        i += 1
