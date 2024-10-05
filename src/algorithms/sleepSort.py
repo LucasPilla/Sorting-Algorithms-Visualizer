0ed0ef58802b37d8c4a06966c5e5d07748121f88
carloschavez <cforcarlos22@gmail.com>
2024-10-05 15:27:36 -0400

    sleepSort algorithm added

---
+++ src/algorithms/sleepSort.py
@@ -0,0 +1,24 @@
+def sleepSort(array, left, right):
+   '''
+   time complexity: O(maxNum * n)
+   '''
+    place = [0]
+    maxNum = max(array)
+    
+    copyArray = array.copy()
+    # Sort the events based on sleep_time
+
+    #time passing
+    for i in range(maxNum + 1):
+        for j in range(len(copyArray)):
+            if copyArray[j] == i:
+                array[place[0]] = copyArray[j]
+                yield array, place[0], j, -1, -1
+                print(f"{array}: {place[0]}")
+                place[0] += 1
+
+
+    # Yield the final sorted array
+    yield array, -1, -1, -1, -1
+    print("Final array")
+    print(array)
